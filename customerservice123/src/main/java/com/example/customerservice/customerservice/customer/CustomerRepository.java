1b1c87818f4e5d07586a5f2e09afd75211e0e989
Elieth <>
2023-02-10 15:10:17 +0300

    deleted customer service 1233

--- customerservice123/src/main/java/com/example/customerservice/customerservice/customer/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.customerservice.customerservice.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-    
-}
--- customerservice123/src/main/java/com/example/customerservice/customerservice/customer/CustomerService.java
+++
@@ -1,5 +0,0 @@
-package com.example.customerservice.customerservice.customer;
-
-public class CustomerService {
-    
-}
